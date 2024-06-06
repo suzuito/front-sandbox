057d772f1e821b72c2555a2a516f873df4976310
鈴木泰斗 <suzukitaito@suzukitaitos-MacBook-Pro-2.local>
2024-06-06 18:32:55 +0900

    save

--- typescript/es2022.amd/hello.js
+++
@@ -1,9 +0,0 @@
-define(["require", "exports"], function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    exports.sayHello = void 0;
-    function sayHello() {
-        console.log("hello");
-    }
-    exports.sayHello = sayHello;
-});
