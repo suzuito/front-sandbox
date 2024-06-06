b4a56cd2876aa4c6f6458ed55d1cc03175d1caa9
鈴木泰斗 <suzukitaito@suzukitaitos-MacBook-Pro-2.local>
2024-06-06 10:46:53 +0900

    save

---
+++ tsconfig_target_and_module/es2022.amd/main.js
@@ -0,0 +1,28 @@
+define(["require", "exports", "./hello"], function (require, exports, hello_1) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    (0, hello_1.sayHello)();
+    class A {
+        hoge;
+        fuga;
+        constructor(hoge = "a", fuga = 100) {
+            this.hoge = hoge;
+            this.fuga = fuga;
+        }
+        bar() {
+            return true;
+        }
+    }
+    const a = new A();
+    let b = a;
+    var c = 0.01;
+    class B extends A {
+        constructor() {
+            super();
+        }
+        foo() { }
+        bar() {
+            return super.bar();
+        }
+    }
+});
