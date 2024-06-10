a04b4b8f4666ef334b1d62088076b5efdc0f3fc5
鈴木泰斗 <suzukitaito@suzukitaitos-MacBook-Pro-2.local>
2024-06-10 14:28:46 +0900

    save

--- typescript_module_es2020/README.md
+++ typescript_module_es2020/README.md
@@ -11,4 +11,15 @@ npx tsc
 
 # 当然、下記は実行できない。エラーとなる。
 node main.js
+
+# バンドラー(webpack)によって中間処理させ、Node.jsで実行できるようにしてみる。
+# (注意)
+#   本レポジトリのwebpack利用例はあまり良くないので、真似しないでください。
+#   webpackによる中間処理を実施することを目的としたJavaScriptをTypeScriptによって出力させる場合、
+#   tsconfigのmoduleを`es2020`にすることは、あまり一般的ではないらしい。
+#   tsconfigのmoduleを`nodeXXXX`にすることが一般的っぽい。
+#     https://webpack.js.org/api/module-methods/
+#   あるいは、TypeScriptをwebpackで処理させる場合、ts-loaderを使ってしまった方が良い。
+npx webpack
+node dist/bundle.js
 ```
---
+++ typescript_module_es2020/webpack.config.cjs
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./main.js",
+    output: {
+        filename: "./bundle.js",
+        path: path.join(__dirname, "dist"),
+    },
+    // バンドル後のJavaScriptの実行環境はNode.js v20
+    target: "node20",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                resolve: {
+                    // import文のモジュール指定子にて、.jsを省略するための設定
+                    fullySpecified: false
+                }
+            }
+        ]
+    }
+}
